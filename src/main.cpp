2ef1e40f089811a76756ae1adec5225d366e34dc
manuelpr <manuelpr@ucm.es>
2024-06-24 21:42:54 +0200

    Encapsulamiento los apartados dentro del main en distintas funciones
    Renderizado de los botones y la pantalla e inicialización de los botones fuera del bucle principal

--- src/main.cpp
+++ src/main.cpp
@@ -121,7 +121,165 @@ bool LoadTextureFromFile(const wchar_t* filename, ID3D11Device* device, ID3D11De
 	return true;
 }
 
+void InitButtons(int windowWidth, int windowHeight, std::vector<Button*>& buttons, std::vector<CheckBox*>& joycheckboxes, std::vector<Slots*>& saveSlots) {
 
+	buttons.push_back(new Button("B: A", (windowWidth) * 0.88f, (windowHeight) * 0.58f, ButtonA)); //A
+	buttons.push_back(new Button("B: B", (windowWidth) * 0.88f, (windowHeight) * 0.66f, ButtonB)); //B
+	buttons.push_back(new Button("B: X", (windowWidth) * 0.88f, (windowHeight) * 0.50f, ButtonX)); //X
+	buttons.push_back(new Button("B: Y", (windowWidth) * 0.88f, (windowHeight) * 0.42f, ButtonY)); //Y
+	buttons.push_back(new Button("B: UP", (windowWidth) * 0.1f, (windowHeight) * 0.55f, DpadUp)); //UP
+	buttons.push_back(new Button("B: DOWN", (windowWidth) * 0.1f, (windowHeight) * 0.65f, DpadDown)); //DOWN
+	buttons.push_back(new Button("B: RIGHT", (windowWidth) * 0.1f, (windowHeight) * 0.70f, DpadRight)); //RIGHT
+	buttons.push_back(new Button("B: LEFT", (windowWidth) * 0.1f, (windowHeight) * 0.60f, DpadLeft)); //LEFT
+	buttons.push_back(new Button("B: LSL", (windowWidth) * 0.305f, (windowHeight) * 0.82f, LeftJoystickLeft)); //Left direction left joystick
+	buttons.push_back(new Button("B: LSR", (windowWidth) * 0.38f, (windowHeight) * 0.82f, LeftJoystickRight)); //Right direction left joystick
+	buttons.push_back(new Button("B: LSU", (windowWidth) * 0.345f, (windowHeight) * 0.77f, LeftJoystickUp)); //Up direction left joystick
+	buttons.push_back(new Button("B: LSD", (windowWidth) * 0.345f, (windowHeight) * 0.88f, LeftJoystickDown)); //Down direction left joystick
+	buttons.push_back(new Button("B: RSL", (windowWidth) * 0.537f, (windowHeight) * 0.82f, RightJoystickLeft)); //Left direction right joystick
+	buttons.push_back(new Button("B: RSR", (windowWidth) * 0.615f, (windowHeight) * 0.82f, RightJoystickRight)); //Right direction right joystick
+	buttons.push_back(new Button("B: RSU", (windowWidth) * 0.577f, (windowHeight) * 0.77f, RightJoystickUp)); //Up direction right joystick
+	buttons.push_back(new Button("B: RSD", (windowWidth) * 0.577f, (windowHeight) * 0.88f, RightJoystickDown)); //Down direction right joystick
+	buttons.push_back(new Button("B: LS", (windowWidth) * 0.42f, (windowHeight) * 0.88f, LeftJoystickButton)); //Left joystick button
+	buttons.push_back(new Button("B: RS", (windowWidth) * 0.66f, (windowHeight) * 0.88f, RightJoystickButton)); //Right joystick button
+	buttons.push_back(new Button("B: LB", (windowWidth) * 0.1f, (windowHeight) * 0.26f, LB)); //LB
+	buttons.push_back(new Button("B: RB", (windowWidth) * 0.89f, (windowHeight) * 0.26f, RB)); //RB
+	buttons.push_back(new Button("B: LT", (windowWidth) * 0.1f, (windowHeight) * 0.15f, LT)); //LT
+	buttons.push_back(new Button("B: RT", (windowWidth) * 0.89f, (windowHeight) * 0.15f, RT)); //RT
+	buttons.push_back(new Button("B: SELECT", (windowWidth) * 0.4f, (windowHeight) * 0.1f, ViewButton)); //Select
+	buttons.push_back(new Button("B: START", (windowWidth) * 0.58f, (windowHeight) * 0.1f, MenuButton)); //Start
+
+	//Botones de cargar y guardar
+	buttons.push_back(new Button("SAVE", (windowWidth) * 0.05f, (windowHeight) * 0.9f, NotAKey)); //save
+	buttons.push_back(new Button("LOAD", (windowWidth) * 0.15f, (windowHeight) * 0.9f, NotAKey)); //load
+
+	//checkBoxs
+	joycheckboxes.push_back(new CheckBox((windowWidth) * 0.23f, (windowHeight) * 0.88f)); //checkbox izquierdo
+	joycheckboxes.push_back(new CheckBox((windowWidth) * 0.46f, (windowHeight) * 0.88f)); //checkbox derecho
+
+	//saveSlots
+	saveSlots.push_back(new Slots("Slot 1", "controles.txt"));
+	saveSlots.push_back(new Slots("Slot 2", "controles2.txt"));
+	saveSlots.push_back(new Slots("Slot 3", "controles3.txt"));
+
+}
+//le da valores a la ventana y se asegura de que los mensajes están funcionando correctamente
+void ImGuiWindow(const HWND& window) {
+
+
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+
+	ImGui::NewFrame();
+
+	RECT windowRect;
+	GetClientRect(window, &windowRect);
+
+	ImGui::SetNextWindowPos(ImVec2(0.f, 0.f));
+	ImGui::SetNextWindowSize(ImVec2(windowRect.right - windowRect.left, windowRect.bottom - windowRect.top));
+
+}
+
+void RenderBotones(int windowWidth, int windowHeight, std::vector<Button*>& buttons, std::vector<CheckBox*>& joycheckboxes, std::vector<Slots*>& saveSlots, InputMapper* mapper, InputSimulator* simulator, Converter* conv, ImGuiKey detectedKey, bool checkL, bool checkR) {
+	
+	// Color de los botones
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.6f, 0.6f, 0.6f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
+
+	for (int i = 0; i < buttons.size(); i++) {
+		// Posicion del boton
+		ImGui::SetCursorPosX(buttons[i]->GetX()); // Centrar horizontalmente
+		ImGui::SetCursorPosY(buttons[i]->GetY()); // Centrar verticalmente 
+
+		// boton input >>>>>>>>>>>>>>>>>
+		if (ImGui::Button(buttons[i]->GetName().c_str())) {
+			if (buttons[i]->GetName() != "SAVE" && buttons[i]->GetName() != "LOAD") {
+				// Activar la espera del siguiente input
+				buttons[i]->SetWaiting(true);
+				detectedKey = ImGuiKey_None;
+				buttons[i]->SetName("Press any key...");
+			}
+			else if (buttons[i]->GetName() == "SAVE") {
+				//mapper->saveControls(checkL,checkR);
+				ImGui::OpenPopup("SavePop");
+
+
+			}
+			else {
+				/*mapper->loadControls(checkL,checkR);
+				simulator->setRightMovement(checkR);
+				simulator->setLeftMovement(checkL);
+				std::vector<std::pair<KeyboardKey, ControllerLayout>>* b = mapper->getButtons();
+				for (int i = 0; i < b->size(); i++)
+				{
+					if((*b)[i].first!=NONE)
+						buttons[i]->SetName(std::string(ImGui::GetKeyName(conv->convertToImGUiKey((*b)[i].first))));
+					else
+						buttons[i]->SetName(std::string(buttons[i]->GetInitialName()));
+
+				}*/
+				ImGui::OpenPopup("LoadPop");
+
+			}
+		}
+
+		// Mostrar el texto y detectar el siguiente input si est  esperando
+		if (buttons[i]->GetWaiting()) {
+			// Detectar el input del teclado
+
+
+			for (ImGuiKey key = ImGuiKey_NamedKey_BEGIN; key < ImGuiKey_NamedKey_END; key = (ImGuiKey)(key + 1)) {
+				if (ImGui::IsKeyDown(key)) {
+					detectedKey = key;
+					buttons[i]->SetWaiting(false);
+					buttons[i]->SetName(std::string(ImGui::GetKeyName(detectedKey)));
+					mapper->setButton(buttons[i]->GetKey(), conv->convertInput(key));
+
+				}
+			}
+		}
+
+	}
+
+	//checkbox de los joysticks
+
+	ImGui::PushStyleColor(ImGuiCol_CheckMark, ImVec4(0.3f, 0.3f, 0.3f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_FrameBgHovered, ImVec4(0.7f, 0.7f, 0.7f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_FrameBgActive, ImVec4(0.5f, 0.5f, 0.5f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+
+	for (int c = 0; c < joycheckboxes.size(); c++) {
+		ImGui::SetCursorPosX(joycheckboxes[c]->GetX()); // Centrar horizontalmente
+		ImGui::SetCursorPosY(joycheckboxes[c]->GetY()); // Centrar verticalmente 
+		if (c == 0) {
+			ImGui::Text("LeftCheckbox");
+			ImGui::SameLine();
+
+			if (ImGui::Checkbox("##LeftCheckbox", &checkL)) {
+
+				simulator->setLeftMovement(checkL);
+
+			}
+		}
+		if (c == 1) {
+			ImGui::Text("RightCheckbox");
+			ImGui::SameLine();
+
+
+			if (ImGui::Checkbox("##RightCheckbox", &checkR)) {
+
+				simulator->setRightMovement(checkR);
+
+			}
+		}
+
+	}
+
+	//comentario de explicacion
+	ImGui::SetCursorPosX((windowWidth) * 0.3f); // Centrar horizontalmente
+	ImGui::SetCursorPosY((windowHeight) * 0.95f); // Centrar verticalmente 
+	ImGui::Text("click one ckeckbox to capture mouse movement with joystick");
+}
 
 INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 	WNDCLASSEXW wc{};
@@ -244,49 +402,11 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 	int windowWidth = windowRect.right - windowRect.left;
 	int windowHeight = windowRect.bottom - windowRect.top;
 
-
+	//inicialización de botones
 	std::vector<Button*> buttons;
-	buttons.push_back(new Button("B: A", (windowWidth) * 0.88f, (windowHeight) * 0.58f, ButtonA)); //A
-	buttons.push_back(new Button("B: B", (windowWidth) * 0.88f, (windowHeight) * 0.66f, ButtonB)); //B
-	buttons.push_back(new Button("B: X", (windowWidth) * 0.88f, (windowHeight) * 0.50f, ButtonX)); //X
-	buttons.push_back(new Button("B: Y", (windowWidth) * 0.88f, (windowHeight) * 0.42f, ButtonY)); //Y
-	buttons.push_back(new Button("B: UP", (windowWidth) * 0.1f, (windowHeight) * 0.55f,DpadUp)); //UP
-	buttons.push_back(new Button("B: DOWN", (windowWidth) * 0.1f, (windowHeight) * 0.65f, DpadDown)); //DOWN
-	buttons.push_back(new Button("B: RIGHT", (windowWidth) * 0.1f, (windowHeight) * 0.70f, DpadRight)); //RIGHT
-	buttons.push_back(new Button("B: LEFT", (windowWidth) * 0.1f, (windowHeight) * 0.60f, DpadLeft)); //LEFT
-	buttons.push_back(new Button("B: LSL", (windowWidth) * 0.305f, (windowHeight) * 0.82f, LeftJoystickLeft)); //Left direction left joystick
-	buttons.push_back(new Button("B: LSR", (windowWidth) * 0.38f, (windowHeight) * 0.82f, LeftJoystickRight)); //Right direction left joystick
-	buttons.push_back(new Button("B: LSU", (windowWidth) * 0.345f, (windowHeight) * 0.77f, LeftJoystickUp)); //Up direction left joystick
-	buttons.push_back(new Button("B: LSD", (windowWidth) * 0.345f, (windowHeight) * 0.88f, LeftJoystickDown)); //Down direction left joystick
-	buttons.push_back(new Button("B: RSL", (windowWidth) * 0.537f, (windowHeight) * 0.82f, RightJoystickLeft)); //Left direction right joystick
-	buttons.push_back(new Button("B: RSR", (windowWidth) * 0.615f, (windowHeight) * 0.82f, RightJoystickRight)); //Right direction right joystick
-	buttons.push_back(new Button("B: RSU", (windowWidth) * 0.577f, (windowHeight) * 0.77f, RightJoystickUp)); //Up direction right joystick
-	buttons.push_back(new Button("B: RSD", (windowWidth) * 0.577f, (windowHeight) * 0.88f, RightJoystickDown)); //Down direction right joystick
-	buttons.push_back(new Button("B: LS", (windowWidth) * 0.42f, (windowHeight) * 0.88f, LeftJoystickButton)); //Left joystick button
-	buttons.push_back(new Button("B: RS", (windowWidth) * 0.66f, (windowHeight) * 0.88f, RightJoystickButton)); //Right joystick button
-	buttons.push_back(new Button("B: LB", (windowWidth) * 0.1f, (windowHeight) * 0.26f, LB)); //LB
-	buttons.push_back(new Button("B: RB", (windowWidth) * 0.89f, (windowHeight) * 0.26f, RB)); //RB
-	buttons.push_back(new Button("B: LT", (windowWidth) * 0.1f, (windowHeight) * 0.15f, LT)); //LT
-	buttons.push_back(new Button("B: RT", (windowWidth) * 0.89f, (windowHeight) * 0.15f, RT)); //RT
-	buttons.push_back(new Button("B: SELECT", (windowWidth) * 0.4f, (windowHeight) * 0.1f,ViewButton)); //Select
-	buttons.push_back(new Button("B: START", (windowWidth) * 0.58f, (windowHeight) * 0.1f,MenuButton)); //Start
-
-	//Botones de cargar y guardar
-	buttons.push_back(new Button("SAVE", (windowWidth) * 0.05f, (windowHeight) * 0.9f, NotAKey)); //save
-	buttons.push_back(new Button("LOAD", (windowWidth) * 0.15f, (windowHeight) * 0.9f, NotAKey)); //load
-
-
-	//Checkbox
 	std::vector<CheckBox*> joycheckboxes;
-	joycheckboxes.push_back(new CheckBox((windowWidth) * 0.23f, (windowHeight) * 0.88f)); //checkbox izquierdo
-	joycheckboxes.push_back(new CheckBox((windowWidth) * 0.46f, (windowHeight) * 0.88f)); //checkbox derecho
-	
-	//SaveSlots
 	std::vector<Slots*> saveSlots;
-	saveSlots.push_back(new Slots("Slot 1", "controles.txt"));
-	saveSlots.push_back(new Slots("Slot 2", "controles2.txt"));
-	saveSlots.push_back(new Slots("Slot 3", "controles3.txt"));
-	
+	InitButtons(windowWidth, windowHeight, buttons, joycheckboxes, saveSlots);
 
 	bool checkL = false;
 	bool checkR = false;
@@ -301,6 +421,8 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 
 	bool running = true;
 	while (running) {
+		
+
 		MSG msg;
 		while (PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
 			TranslateMessage(&msg); //relacionado con input de teclado
@@ -310,23 +432,9 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 				running = false;
 			}
 		}
-
-		if (!running) {
-			break;
-		}
-
-		ImGui_ImplDX11_NewFrame();
-		ImGui_ImplWin32_NewFrame();
-
-		ImGui::NewFrame();
 		
-		RECT windowRect;
-		GetClientRect(window, &windowRect);
-		int windowWidth = windowRect.right - windowRect.left;
-		int windowHeight = windowRect.bottom - windowRect.top;
+		ImGuiWindow(window);
 
-		ImGui::SetNextWindowPos(ImVec2(0.f, 0.f));
-		ImGui::SetNextWindowSize(ImVec2(windowWidth, windowHeight));
 		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 		//panel de botones
@@ -336,66 +444,10 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 		}
 
 		//imagen del mando
-		ImGui::GetBackgroundDrawList()->AddImage((void*)myTexture.Get(), ImVec2(0, 0), ImVec2(windowWidth, windowHeight));		
+		ImGui::GetBackgroundDrawList()->AddImage((void*)myTexture.Get(), ImVec2(0, 0), ImVec2(windowWidth, windowHeight));
 
-		// Color de los botones
-		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.6f, 0.6f, 0.6f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
-
-		for (int i = 0; i < buttons.size(); i++) {
-			// Posicion del boton
-			ImGui::SetCursorPosX(buttons[i]->GetX()); // Centrar horizontalmente
-			ImGui::SetCursorPosY(buttons[i]->GetY()); // Centrar verticalmente 
-
-			// boton input >>>>>>>>>>>>>>>>>
-			if (ImGui::Button(buttons[i]->GetName().c_str())) {
-				if (buttons[i]->GetName() != "SAVE" && buttons[i]->GetName() != "LOAD") {
-					// Activar la espera del siguiente input
-					buttons[i]->SetWaiting(true);
-					detectedKey = ImGuiKey_None;
-					buttons[i]->SetName("Press any key...");
-				}
-				else if(buttons[i]->GetName() == "SAVE") {
-					//mapper->saveControls(checkL,checkR);
-					ImGui::OpenPopup("SavePop");
-					
-					
-				}
-				else {
-					/*mapper->loadControls(checkL,checkR);
-					simulator->setRightMovement(checkR);
-					simulator->setLeftMovement(checkL);
-					std::vector<std::pair<KeyboardKey, ControllerLayout>>* b = mapper->getButtons();
-					for (int i = 0; i < b->size(); i++)
-					{
-						if((*b)[i].first!=NONE)
-							buttons[i]->SetName(std::string(ImGui::GetKeyName(conv->convertToImGUiKey((*b)[i].first))));
-						else
-							buttons[i]->SetName(std::string(buttons[i]->GetInitialName()));
-						
-					}*/
-					ImGui::OpenPopup("LoadPop");
-					
-				}
-			}
-			
-			// Mostrar el texto y detectar el siguiente input si est  esperando
-			if (buttons[i]->GetWaiting()) {
-				// Detectar el input del teclado
-				
-				
-				for (ImGuiKey key = ImGuiKey_NamedKey_BEGIN; key < ImGuiKey_NamedKey_END; key = (ImGuiKey)(key + 1)) {
-					if (ImGui::IsKeyDown(key)) {
-						detectedKey = key;
-						buttons[i]->SetWaiting(false);
-						buttons[i]->SetName(std::string(ImGui::GetKeyName(detectedKey)));
-						mapper->setButton(buttons[i]->GetKey(), conv->convertInput(key));
-						
-					}
-				}
-			}
-			
-		}
+		//Render de los botones y las checkboxes
+		RenderBotones(windowWidth, windowHeight, buttons, joycheckboxes, saveSlots, mapper, simulator, conv, detectedKey, checkL, checkR);
 
 		//popup para guardar el preset
 		if (ImGui::BeginPopup("SavePop"))
@@ -451,46 +503,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 
 			ImGui::EndPopup();
 		}
-
-		//checkbox de los joysticks
-		 
-		ImGui::PushStyleColor(ImGuiCol_CheckMark, ImVec4(0.3f, 0.3f, 0.3f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_FrameBgHovered, ImVec4(0.7f, 0.7f, 0.7f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_FrameBgActive, ImVec4(0.5f, 0.5f, 0.5f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
-
-		for (int c = 0; c < joycheckboxes.size(); c++) {
-			ImGui::SetCursorPosX(joycheckboxes[c]->GetX()); // Centrar horizontalmente
-			ImGui::SetCursorPosY(joycheckboxes[c]->GetY()); // Centrar verticalmente 
-			if (c == 0) {
-				ImGui::Text("LeftCheckbox");
-				ImGui::SameLine();
-
-				if (ImGui::Checkbox("##LeftCheckbox", &checkL)) {
-
-					simulator->setLeftMovement(checkL);
-
-				}
-			}
-			if (c == 1) {
-				ImGui::Text("RightCheckbox");
-				ImGui::SameLine();
-
-
-				if (ImGui::Checkbox("##RightCheckbox", &checkR)) {
-
-					simulator->setRightMovement(checkR);
-
-				}
-			}
-
-		}
-		
-		//comentario de explicacion
-		ImGui::SetCursorPosX((windowWidth) * 0.3f); // Centrar horizontalmente
-		ImGui::SetCursorPosY((windowHeight) * 0.95f); // Centrar verticalmente 
-		ImGui::Text("click one ckeckbox to capture mouse movement with joystick");
 
 		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 		ImVec2 windowSize = ImGui::GetWindowSize(); //windowSize.x *= 2; windowSize.y *= 2;
