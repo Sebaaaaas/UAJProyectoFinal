5a3ee18a7d20a8f48766e71baaf364394fa8451a
dcasiano <dcasiano@ucm.es>
2024-06-25 17:55:17 +0200

    simulador deberia ir mas fino

--- src/InputSimulator.cpp
+++ src/InputSimulator.cpp
@@ -6,6 +6,7 @@
 InputSimulator::InputSimulator()
 {
 	mapper = nullptr;
+	pressedButtons = std::vector(numButtons, false);
 }
 
 InputSimulator::~InputSimulator()
@@ -19,123 +20,265 @@ void InputSimulator::update()
 	XINPUT_STATE state;
 	ZeroMemory(&state, sizeof(XINPUT_STATE));
 	DWORD result = XInputGetState(0, &state);
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_Y)) {
-		simulateKey(mapper->getKey(ButtonY));
+	if (result != ERROR_SUCCESS)return;
+
+	// Botones principales
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_Y) {
+		simulateKeyPressed(mapper->getKey(ButtonY));
+		pressedButtons[ButtonY] = true;
+		
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_X)) {
-		simulateKey(mapper->getKey(ButtonX));
+	else if (pressedButtons[ButtonY]) {
+		simulateKeyReleased(mapper->getKey(ButtonY));
+		pressedButtons[ButtonY] = false;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_B)) {
-		simulateKey(mapper->getKey(ButtonB));
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_X) {
+		simulateKeyPressed(mapper->getKey(ButtonX));
+		pressedButtons[ButtonX] = true;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_A)) {
-		simulateKey(mapper->getKey(ButtonA));
+	else if (pressedButtons[ButtonX]) {
+		simulateKeyReleased(mapper->getKey(ButtonX));
+		pressedButtons[ButtonX] = false;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP)) {
-		simulateKey(mapper->getKey(DpadUp));
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_A) {
+		simulateKeyPressed(mapper->getKey(ButtonA));
+		pressedButtons[ButtonA] = true;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN)) {
-		simulateKey(mapper->getKey(DpadDown));
+	else if (pressedButtons[ButtonA]) {
+		simulateKeyReleased(mapper->getKey(ButtonA));
+		pressedButtons[ButtonA] = false;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT)) {
-		simulateKey(mapper->getKey(DpadLeft));
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_B) {
+		simulateKeyPressed(mapper->getKey(ButtonB));
+		pressedButtons[ButtonB] = true;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT)) {
-		simulateKey(mapper->getKey(DpadRight));
+	else if (pressedButtons[ButtonB]) {
+		simulateKeyReleased(mapper->getKey(ButtonB));
+		pressedButtons[ButtonB] = false;
 	}
-	if (result == ERROR_SUCCESS && movRight) {
+
+	// Dpad
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP) {
+		simulateKeyPressed(mapper->getKey(DpadUp));
+		pressedButtons[DpadUp] = true;
+	}
+	else if (pressedButtons[DpadUp]) {
+		simulateKeyReleased(mapper->getKey(DpadUp));
+		pressedButtons[DpadUp] = false;
+	}
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN) {
+		simulateKeyPressed(mapper->getKey(DpadDown));
+		pressedButtons[DpadDown] = true;
+	}
+	else if (pressedButtons[DpadDown]) {
+		simulateKeyReleased(mapper->getKey(DpadDown));
+		pressedButtons[DpadDown] = false;
+	}
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT) {
+		simulateKeyPressed(mapper->getKey(DpadLeft));
+		pressedButtons[DpadLeft] = true;
+	}
+	else if (pressedButtons[DpadLeft]) {
+		simulateKeyReleased(mapper->getKey(DpadLeft));
+		pressedButtons[DpadLeft] = false;
+	}
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT) {
+		simulateKeyPressed(mapper->getKey(DpadRight));
+		pressedButtons[DpadRight] = true;
+	}
+	else if (pressedButtons[DpadRight]) {
+		simulateKeyReleased(mapper->getKey(DpadRight));
+		pressedButtons[DpadRight] = false;
+	}
+	
+	// Joysticks
+	if (movRight) {
 		// Obtén los valores del joystick derecho
 		short thumbRX = state.Gamepad.sThumbRX;
 		short thumbRY = state.Gamepad.sThumbRY;
 
 		// Verifica si el joystick derecho se está moviendo
-		if (thumbRX != 0 || thumbRY != 0) {
+		if (abs(thumbRX) > joystickThreshold || abs(thumbRY) > joystickThreshold) {
 			// Llama a una función para simular el movimiento del ratón
 			simulateMouseMove(thumbRX, thumbRY);
 		}
 	}
-
-	else if (result == ERROR_SUCCESS) {
+	else {
 		// Obtén los valores del joystick izquierdo
 		short thumbRX = state.Gamepad.sThumbRX;
 		short thumbRY = state.Gamepad.sThumbRY;
 
 		// Verifica si el joystick izquierdo se está moviendo
-		if (thumbRX > 0) {
-			// Llama a una función para simular el movimiento del ratón
-			simulateKey(mapper->getKey(RightJoystickRight));
+		if (thumbRX > joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(RightJoystickRight));
+			pressedButtons[RightJoystickRight] = true;
+			
 		}
-		else if (thumbRX < 0) {
-			simulateKey(mapper->getKey(RightJoystickLeft));
+		else if (pressedButtons[RightJoystickRight]) {
+			simulateKeyReleased(mapper->getKey(RightJoystickRight));
+			pressedButtons[RightJoystickRight] = false;
 		}
-		if (thumbRY > 0) {
-			simulateKey(mapper->getKey(RightJoystickUp));
+
+		if (thumbRX < -joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(RightJoystickLeft));
+			pressedButtons[RightJoystickLeft] = true;
 		}
-		else if (thumbRY < 0) {
-			simulateKey(mapper->getKey(RightJoystickDown));
+		else if (pressedButtons[RightJoystickLeft]) {
+			simulateKeyReleased(mapper->getKey(RightJoystickLeft));
+			pressedButtons[RightJoystickLeft] = false;
+		}
+
+		if (thumbRY > joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(RightJoystickUp));
+			pressedButtons[RightJoystickUp] = true;
+
+		}
+		else if (pressedButtons[RightJoystickUp]) {
+			simulateKeyReleased(mapper->getKey(RightJoystickUp));
+			pressedButtons[RightJoystickUp] = false;
+		}
+
+		if (thumbRY < -joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(RightJoystickDown));
+			pressedButtons[RightJoystickDown] = true;
+		}
+		else if (pressedButtons[RightJoystickDown]) {
+			simulateKeyReleased(mapper->getKey(RightJoystickDown));
+			pressedButtons[RightJoystickDown] = false;
 		}
 	}
 
-	if (result == ERROR_SUCCESS && movLeft) {
+	if (movLeft) {
 		// Obtén los valores del joystick izquierdo
 		short thumbLX = state.Gamepad.sThumbLX;
 		short thumbLY = state.Gamepad.sThumbLY;
 
 		// Verifica si el joystick izquierdo se está moviendo
-		if (thumbLX != 0 || thumbLY != 0) {
+		if (abs(thumbLX) > joystickThreshold || abs(thumbLY) > joystickThreshold) {
 			// Llama a una función para simular el movimiento del ratón
 			simulateMouseMove(thumbLX, thumbLY);
 		}
 	}
-
-
-	else if (result == ERROR_SUCCESS) {
+	else {
 		// Obtén los valores del joystick izquierdo
 		short thumbLX = state.Gamepad.sThumbLX;
 		short thumbLY = state.Gamepad.sThumbLY;
 
 		// Verifica si el joystick izquierdo se está moviendo
-		if (thumbLX > 0) {
-			// Llama a una función para simular el movimiento del ratón
-			simulateKey(mapper->getKey(LeftJoystickRight));
+		if (thumbLX > joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(LeftJoystickRight));
+			pressedButtons[LeftJoystickRight] = true;
 		}
-		else if (thumbLX < 0) {
-			simulateKey(mapper->getKey(LeftJoystickLeft));
+		else if (pressedButtons[LeftJoystickRight]) {
+			simulateKeyReleased(mapper->getKey(LeftJoystickRight));
+			pressedButtons[LeftJoystickRight] = false;
 		}
-		if (thumbLY > 0) {
-			simulateKey(mapper->getKey(LeftJoystickUp));
+
+		if (thumbLX < -joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(LeftJoystickLeft));
+			pressedButtons[LeftJoystickLeft] = true;
 		}
-		else if (thumbLY < 0) {
-			simulateKey(mapper->getKey(LeftJoystickDown));
+		else if (pressedButtons[LeftJoystickLeft]) {
+			simulateKeyReleased(mapper->getKey(LeftJoystickLeft));
+			pressedButtons[LeftJoystickLeft] = false;
+		}
+
+		if (thumbLY > joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(LeftJoystickUp));
+			pressedButtons[LeftJoystickUp] = true;
+		}
+		else if (pressedButtons[LeftJoystickUp]) {
+			simulateKeyReleased(mapper->getKey(LeftJoystickUp));
+			pressedButtons[LeftJoystickUp] = false;
+		}
+
+		if (thumbLY < -joystickThreshold) {
+			simulateKeyPressed(mapper->getKey(LeftJoystickDown));
+			pressedButtons[LeftJoystickDown] = true;
+		}
+		else if (pressedButtons[LeftJoystickDown]) {
+			simulateKeyReleased(mapper->getKey(LeftJoystickDown));
+			pressedButtons[LeftJoystickDown] = false;
 		}
 	}
 
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER)) { //Pulsar LB 
-		simulateKey(mapper->getKey(LB));
+	// Gatillos, shoulders, botones de joysticks y demas botones
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER) { //Pulsar LB 
+		simulateKeyPressed(mapper->getKey(LB));
+		pressedButtons[LB] = true;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_THUMB)) { //Pulsar joystick izquierdo
-		simulateKey(mapper->getKey(LeftJoystickButton));
-	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER)) { //Pulsar RB 
-		simulateKey(mapper->getKey(RB));
-	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_THUMB)) { //Pulsar joystick derecho
-		simulateKey(mapper->getKey(RightJoystickButton));
-	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.bLeftTrigger)) { //Pulsar LT 
-		if (state.Gamepad.bLeftTrigger > 75)
-			simulateKey(mapper->getKey(LT));
-	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.bRightTrigger)) { //Pulsar RT
-		if (state.Gamepad.bRightTrigger > 75)
-			simulateKey(mapper->getKey(RT));
+	else if (pressedButtons[LB]) {
+		simulateKeyReleased(mapper->getKey(LB));
+		pressedButtons[LB] = false;
 	}
 
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_START)) { //Pulsar joystick derecho
-		simulateKey(mapper->getKey(MenuButton));
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_THUMB) { //Pulsar joystick izquierdo 
+		simulateKeyPressed(mapper->getKey(LeftJoystickButton));
+		pressedButtons[LeftJoystickButton] = true;
 	}
-	if (result == ERROR_SUCCESS && (state.Gamepad.wButtons & XINPUT_GAMEPAD_BACK)) { //Pulsar joystick derecho
-		simulateKey(mapper->getKey(ViewButton));
+	else if (pressedButtons[LeftJoystickButton]) {
+		simulateKeyReleased(mapper->getKey(LeftJoystickButton));
+		pressedButtons[LeftJoystickButton] = false;
+	}
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) { //Pulsar RB  
+		simulateKeyPressed(mapper->getKey(RB));
+		pressedButtons[RB] = true;
+	}
+	else if (pressedButtons[RB]) {
+		simulateKeyReleased(mapper->getKey(RB));
+		pressedButtons[RB] = false;
+	}
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_THUMB) { //Pulsar joystick derecho 
+		simulateKeyPressed(mapper->getKey(RightJoystickButton));
+		pressedButtons[RightJoystickButton] = true;
+	}
+	else if (pressedButtons[RightJoystickButton]) {
+		simulateKeyReleased(mapper->getKey(RightJoystickButton));
+		pressedButtons[RightJoystickButton] = false;
+	}
+
+	if (state.Gamepad.bLeftTrigger && state.Gamepad.bLeftTrigger > 75) { //Pulsar LT 
+		simulateKeyPressed(mapper->getKey(LT));
+		pressedButtons[LT] = true;
+	}
+	else if (pressedButtons[LT]) {
+		simulateKeyReleased(mapper->getKey(LT));
+		pressedButtons[LT] = false;
+	}
+
+	if (state.Gamepad.bRightTrigger && state.Gamepad.bRightTrigger > 75) { //Pulsar RT 
+		simulateKeyPressed(mapper->getKey(RT));
+		pressedButtons[RT] = true;
+	}
+	else if (pressedButtons[RT]) {
+		simulateKeyReleased(mapper->getKey(RT));
+		pressedButtons[RT] = false;
+	}
+	
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_START) { //Pulsar boton menu
+		simulateKeyPressed(mapper->getKey(MenuButton));
+		pressedButtons[MenuButton] = true;
+	}
+	else if (pressedButtons[MenuButton]) {
+		simulateKeyReleased(mapper->getKey(MenuButton));
+		pressedButtons[MenuButton] = false;
+	}
+
+	if (state.Gamepad.wButtons & XINPUT_GAMEPAD_BACK) { //Pulsar boton view
+		simulateKeyPressed(mapper->getKey(ViewButton));
+		pressedButtons[ViewButton] = true;
+	}
+	else if (pressedButtons[ViewButton]) {
+		simulateKeyReleased(mapper->getKey(ViewButton));
+		pressedButtons[ViewButton] = false;
 	}
 }
 
@@ -144,11 +287,11 @@ void InputSimulator::setMapper(InputMapper* mapper)
 	this->mapper = mapper;
 }
 
-void InputSimulator::simulateKey(KeyboardKey key)
+void InputSimulator::simulateKeyPressed(KeyboardKey key)
 {
 	if (key == NONE)return;
 
-	if (simulateMouseKey(key))return;
+	if (simulateMouseKeyPressed(key))return;
 
 	INPUT input;
 	ZeroMemory(&input, sizeof(INPUT));
@@ -156,12 +299,23 @@ void InputSimulator::simulateKey(KeyboardKey key)
 	input.ki.wVk = key;
 	input.ki.dwFlags = 0;
 	SendInput(1, &input, sizeof(INPUT));
-	Sleep(50);
+}
+
+void InputSimulator::simulateKeyReleased(KeyboardKey key)
+{
+	if (key == NONE)return;
+
+	if (simulateMouseKeyReleased(key))return;
+
+	INPUT input;
+	ZeroMemory(&input, sizeof(INPUT));
+	input.type = INPUT_KEYBOARD;
+	input.ki.wVk = key;
 	input.ki.dwFlags = KEYEVENTF_KEYUP;
 	SendInput(1, &input, sizeof(INPUT));
 }
 
-bool InputSimulator::simulateMouseKey(KeyboardKey key)
+bool InputSimulator::simulateMouseKeyPressed(KeyboardKey key)
 {
 	if (key == NONE || (key != KEY_MLCLICK && key != KEY_MRCLICK && key != KEY_MMIDDLEBUTTON))return false;
 
@@ -174,6 +328,16 @@ bool InputSimulator::simulateMouseKey(KeyboardKey key)
 	else input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN; // Presiona el botón central del ratón
 
 	SendInput(1, &input, sizeof(INPUT));
+	return true;
+}
+
+bool InputSimulator::simulateMouseKeyReleased(KeyboardKey key)
+{
+	if (key == NONE || (key != KEY_MLCLICK && key != KEY_MRCLICK && key != KEY_MMIDDLEBUTTON))return false;
+
+	INPUT input;
+	ZeroMemory(&input, sizeof(INPUT));
+	input.type = INPUT_MOUSE;
 
 	if (key == KEY_MLCLICK)input.mi.dwFlags = MOUSEEVENTF_LEFTUP; // Suelta el botón izquierdo del ratón
 	else if (key == KEY_MRCLICK)input.mi.dwFlags = MOUSEEVENTF_RIGHTUP; // Suelta el botón derecho del ratón
--- src/InputSimulator.h
+++ src/InputSimulator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "DefInput.h"
+#include <vector>
 
 class InputMapper;
 class InputSimulator
@@ -13,13 +14,20 @@ public:
 	void setLeftMovement(bool leftMov);
 	void setRightMovement(bool rightMov);
 private:
-	void simulateKey(KeyboardKey key);
-	bool simulateMouseKey(KeyboardKey key);
+	void simulateKeyPressed(KeyboardKey key);
+	void simulateKeyReleased(KeyboardKey key);
+	bool simulateMouseKeyPressed(KeyboardKey key);
+	bool simulateMouseKeyReleased(KeyboardKey key);
 	void simulateMouseMove(short deltaX, short deltaY);
 
 
 	InputMapper* mapper;
 	bool movRight = false;
 	bool movLeft = false;
+	const short joystickThreshold = 10000;
+
+	// Numero de botones del mando que se pueden simular
+	const int numButtons = 24;
+	std::vector<bool>pressedButtons;
 };
 
